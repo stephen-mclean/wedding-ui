9a36a3afcd84b3302ab6692426b7b2c576f1c713
Stephen McLean <stephanmclean93@gmail.com>
2025-01-26 16:20:34 +0000

    fix back button

--- src/app/[code]/invite-form.tsx
+++ src/app/[code]/invite-form.tsx
@@ -14,6 +14,7 @@ import { useRouter } from "next/navigation";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TextArea } from "@/components/ui/textarea";
+import Link from "next/link";
 
 const guestSchema = z.object({
   isAttending: z.boolean(),
@@ -128,9 +129,11 @@ export const InviteForm = ({ invite }: Props) => {
         />
 
         <div className="flex justify-end gap-2">
-          <Button variant="secondary" type="button">
-            Back
-          </Button>
+          <Link href="/">
+            <Button variant="secondary" type="button">
+              Back
+            </Button>
+          </Link>
           <Button type="submit" disabled={isDisabled}>
             Submit
           </Button>
