46474bee87943b0e4d5491d2f1325f6c9a8bb181
Stephen McLean <stephanmclean93@gmail.com>
2025-01-27 12:04:49 +0000

    small styling changes

--- src/app/rsvp/page.tsx
+++ src/app/rsvp/page.tsx
@@ -2,8 +2,14 @@ import RSVPForm from "./form";
 
 export default function RSVP() {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 flex-1 lg:max-w-lg">
       <h1 className="font-serif text-4xl text-center">RSVP</h1>
+      <p className="font-sans text-base">
+        You can find your invite code on the back of your invitation printed on
+        the same sticker as the QR code. Scanning the QR code will take you to
+        your invite automatically, but you can type in the code manually here if
+        you like.
+      </p>
       <RSVPForm />
     </div>
   );
